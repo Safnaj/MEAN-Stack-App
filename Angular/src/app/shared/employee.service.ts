b3c2441251b02d151762e303dbf694e80351d8c9
Safnaj <safnaj99@gmail.com>
2019-09-16 19:31:10 +0530

    Backend Services

--- Angular/src/app/shared/employee.service.ts
+++ Angular/src/app/shared/employee.service.ts
@@ -1,19 +1,24 @@
   
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 //import 'rxjs/add/operator/map';
 //import 'rxjs/add/operator/toPromise';
 import { map } from 'rxjs/operators';
+//import { toPromise } from 'rxjs/operator';
 
 import { Employee } from './employee.model';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class EmployeeService {
   selectedEmployee: Employee;
   employees: Employee[];
 
+  readonly baseURL = 'http://localhost:300/employees';
+
   constructor(private http: HttpClient) { }
+
+  postEmployee(emp : Employee){
+    return this.http.post(this.baseURL, emp);
+  }
 }
