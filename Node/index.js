cf99cb57f8e5b86834dddd5c02cc323d1a388102
Safnaj <safnaj99@gmail.com>
2019-09-16 19:34:52 +0530

    Backend Services

--- Node/index.js
+++ Node/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const {mongoose} = require('./db.js');
 var EmployeeController = require('./controllers/EmployeeController.js');
@@ -7,8 +8,11 @@ var EmployeeController = require('./controllers/EmployeeController.js');
 var app = express();
 app.use(bodyParser.json());
 
+//Allows Frontend Server URL
+app.use(cors({origin: 'http://localhost:4200'}));
+
 app.listen(3000,() => console.log('Server Started at port : 3000'));
 
-
+//API url returns controller
 app.use('/employees', EmployeeController);
 
